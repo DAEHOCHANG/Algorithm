333480b17efc54d9d1d3b838bfb52fd471bd45d5
DAEHOCHANG <eogh1209@gmail.com>
2021-02-12 22:06:32 +0900

    3053

---
+++ Algorithm/3053/3053.swift
@@ -0,0 +1,15 @@
+//
+//  3053.swift
+//  Algorithm
+//
+//  Created by 장대호 on 2021/02/12.
+//
+
+import Foundation
+
+func _3053() {
+    let pi = 3.14159265359
+    let num = Double(readLine()!)!
+    print(num*num*pi)
+    print(num*num*2)
+}
